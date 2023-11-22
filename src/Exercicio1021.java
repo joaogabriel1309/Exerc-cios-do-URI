a97ba294c893ab3c2f12db37e138c2a52ede2ee8
Joao.Gabriel <gabriel.122.gm85@gmail.com>
2023-11-21 22:49:34 -0400

    Organizei melhor as pastas deste repositório de exercícios do URI e adicionei o exercício 1026.

--- src/Exercicio1021.java
+++ src/Exercicio1021.java
@@ -1,8 +1,27 @@
+/*
+
+ QUESTÃO
+ Leia um valor de ponto flutuante com duas casas decimais. Este valor
+ representa um valor monetário. A seguir, calcule o menor número de notas
+ e moedas possíveis no qual o valor pode ser decomposto. As notas
+ consideradas são de 100, 50, 20, 10, 5, 2. As moedas possíveis são
+ de 1, 0.50, 0.25, 0.10, 0.05 e 0.01. A seguir mostre a relação de notas necessárias.
+
+ ENTRADA
+ O arquivo de entrada contém um valor de ponto flutuante N (0 <= N <= 1000000.00).
+
+ SAÍDA
+ Imprima a quantidade mínima de notas e moedas necessárias para trocar o valor
+ inicial, conforme exemplo fornecido.
+
+ Obs: Utilize ponto (.) para separar a parte decimal.
+
+*/
+
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
-
+public class Exercicio1021 {
     public static void main(String[] args) {
         // Constantes para notas e moedas
         final double[] NOTAS_E_MOEDAS = {100.00, 50.00, 20.00, 10.00, 5.00, 2.00, 1.00, 0.50, 0.25, 0.10, 0.05, 0.01};
