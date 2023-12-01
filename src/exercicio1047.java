377af3e945fc5d2d57449c2eec7fa90d8670ac21
Joao.Gabriel <gabriel.122.gm85@gmail.com>
2023-12-01 00:12:49 -0400

    exercicio1047

---
+++ src/exercicio1047.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class exercicio1047 {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        int StartHours = scr.nextInt();
+        int StartMinute = scr.nextInt();
+        int FinalHours = scr.nextInt();
+        int FinalMinute = scr.nextInt();
+
+        double result = 0;
+
+        if ((FinalMinute > StartMinute) && (StartHours == FinalHours)) {
+            result = FinalMinute - StartMinute;
+        }else{
+            if ((StartMinute > FinalMinute) && (StartHours == FinalHours)){
+                result = 1440 - (StartMinute - FinalMinute);
+            }else{
+                if (StartHours > FinalHours){
+                    result = (long) (StartHours - FinalHours) * 60;
+                    result = 1440 - result;
+                    if (StartMinute > FinalMinute){
+                        result = result - (StartMinute - FinalMinute);
+                    }else{
+                        result = result + (FinalMinute - StartMinute);
+                    }
+                }else{
+                    result = (long) (FinalHours - StartHours) * 60;
+                    if (StartMinute > FinalMinute){
+                        result = result - (StartMinute - FinalMinute);
+                    }else{
+                        result = result + (FinalMinute - StartMinute);
+                    }
+                }
+            }
+
+            int resultHours = (int) (result / 60);
+            int resultMinutes = (int) (result % 60);
+            if ((StartHours == FinalHours) && (resultHours == 0)){
+                resultHours = 24;
+            }
+
+            System.out.println("O JOGO DUROU "+ resultHours+ " HORA(S) E "+resultMinutes+" MINUTO(S)");
+        }
+    }
+}
