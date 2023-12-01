a9a23b8a2e48e8aed745ab2d555854938a704f49
Joao.Gabriel <gabriel.122.gm85@gmail.com>
2023-11-30 23:45:17 -0400

    Exercicio1009

---
+++ src/Exercicio1009.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Exercicio1009 {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        String Name = scr.next();
+        double FixedSalry = scr.nextDouble();;
+        double Sales = scr.nextDouble();
+        double Bonus =  Sales * 0.15;
+        double Total = FixedSalry + Bonus;
+        System.out.println("TOTAL = R$ "+String.format("%2.2f", Total));
+        scr.close();
+    }
+}
