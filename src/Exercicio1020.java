744e3bc09b4ab7a5569a9e6220aaa80fc94bb932
Joao.Gabriel <gabriel.122.gm85@gmail.com>
2023-12-06 23:23:28 -0400

    Exercicio1020, correções no codigo

--- src/Exercicio1020.java
+++ src/Exercicio1020.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Exercicio1020 {
 
-    public static void main(String[] args)  throws IOException {
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
         Locale.setDefault(new Locale("en", "US"));
