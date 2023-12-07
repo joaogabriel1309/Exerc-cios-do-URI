69f5a0a91d3a772c134cf69820ddb25722642574
Joao.Gabriel <gabriel.122.gm85@gmail.com>
2023-12-06 23:24:53 -0400

    Exercicio1020, correções no codigo

--- src/Exercicio1020.java
+++ src/Exercicio1020.java
@@ -1,31 +1,30 @@
-import java.io.IOException;import java.util.Locale;
+import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercicio1020 {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
         Locale.setDefault(new Locale("en", "US"));
 
-        int Age,Year,Month;
+        int Age, Year, Month;
         Age = sc.nextInt();
 
-        if (Age >= 365){
+        if (Age >= 365) {
             Year = Age / 365;
             Age = Age % 365;
             System.out.printf("%d ano(s)\n", Year);
-        }
-        else{
+        } else {
             System.out.print("0 ano(s)\n");
         }
 
-        if (Age >= 30){
+        if (Age >= 30) {
             Month = Age / 30;
             Age = Age % 30;
             System.out.printf("%d mes(es)\n", Month);
-        }
-        else{
+        } else {
             System.out.print("0 mes(es)\n");
         }
 
