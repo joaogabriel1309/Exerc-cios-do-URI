38f6b81042687ee7de6fcb8d0e9e92e2c311118e
Joao.Gabriel <gabriel.122.gm85@gmail.com>
2023-12-20 00:00:04 -0400

    Exercicio1192

--- src/Exercicio1038.java
+++ src/Exercicio1038.java
@@ -3,27 +3,32 @@ import java.util.Scanner;
 public class Exercicio1038 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        sc.close();
-        double r = 0;
-        switch (a) {
+
+        int fValue1 = sc.nextInt();
+        int fValue2 = sc.nextInt();
+
+        double result = 0;
+        switch (fValue1) {
             case 1:
-                r = b * 4.00;
+                result = fValue2 * 4.00;
                 break;
             case 2:
-                r = b * 4.50;
+                result = fValue2 * 4.50;
                 break;
             case 3:
-                r = b * 5.00;
+                result = fValue2 * 5.00;
                 break;
             case 4:
-                r = b * 2.00;
+                result = fValue2 * 2.00;
                 break;
             case 5:
-                r = b * 1.50;
+                result = fValue2 * 1.50;
+                break;
+            default:
                 break;
         }
-        System.out.printf("Total: R$ %.2f\n", r);
+        System.out.printf("Total: R$ %.2f\n", result);
+
+        sc.close();
     }
 }
