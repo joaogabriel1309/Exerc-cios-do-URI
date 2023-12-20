738bea9657fcb397bd1c248c0484980bf6c347af
Joao.Gabriel <gabriel.122.gm85@gmail.com>
2023-12-19 23:53:36 -0400

    Exercicio1192

--- src/Exercicio1192.java
+++ src/Exercicio1192.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class Exercicio1192 {
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
-        int N = scr.nextInt();
-        for (int i = 0; i < N; i++) {
+        int fNumero = scr.nextInt();
+        for (int i = 0; i < fNumero; i++) {
             String linha = scr.next();
-            int x = Character.getNumericValue(linha.charAt(0));
-            char z = linha.charAt(1);
-            int y = Character.getNumericValue(linha.charAt(2));
-            if (x == y) {
-                System.out.println(x * y);
-            } else if (Character.isUpperCase(z)) {
-                System.out.println(y - x);
+            int fPosition1 = Character.getNumericValue(linha.charAt(0));
+            char fPosition2 = linha.charAt(1);
+            int fPosition3 = Character.getNumericValue(linha.charAt(2));
+            if (fPosition1 == fPosition3) {
+                System.out.println(fPosition1 * fPosition3);
+            } else if (Character.isUpperCase(fPosition2)) {
+                System.out.println(fPosition3 - fPosition1);
             } else {
-                System.out.println(x + y);
+                System.out.println(fPosition1 + fPosition3);
             }
         }
     }
