df361ad22ac40e4d3fedb9179476864e866e9182
Joao.Gabriel <gabriel.122.gm85@gmail.com>
2023-12-22 21:50:36 -0400

    Exercicio1047

--- src/exercicio1047.java
+++ src/exercicio1047.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class exercicio1047 {
+public class Exercicio1047 {
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
         int StartHours = scr.nextInt();
