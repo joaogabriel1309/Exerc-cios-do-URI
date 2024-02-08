ebbe470f27411f2c7168550e1bb347938d512f5e
joao <gabriel.122.gm85@gmail.com>
2024-02-07 23:04:59 -0300

    Exercicio1050

--- src/Exercicio1050.java
+++ src/Exercicio1050.java
@@ -2,19 +2,20 @@ import java.util.Scanner;
 
 public class Exercicio1050 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a[] = {61, 71, 11, 21, 32, 19, 27, 31};
-        String b[] = {"Brasilia", "Salvador", "Sao Paulo", "Rio de Janeiro", "Juiz de Fora", "Campinas", "Vitoria", "Belo Horizonte"};
-        int c = sc.nextInt();
-        int j = 0, i;
+        Scanner scr = new Scanner(System.in);
+        int number[] = {61, 71, 11, 21, 32, 19, 27, 31};
+        String city[] = {"Brasilia", "Salvador", "Sao Paulo", "Rio de Janeiro", "Juiz de Fora", "Campinas", "Vitoria", "Belo Horizonte"};
+        int input = scr.nextInt();
+
+        int controller = 0, i;
         for (i = 0; i < 8; i++) {
-            if (c == a[i]) {
-                System.out.println(b[i]);
+            if (input == number[i]) {
+                System.out.println(city[i]);
             } else {
-                j++;
+                controller++;
             }
         }
-        if (i == j) {
+        if (i == controller) {
             System.out.printf("DDD nao cadastrado\n");
         }
     }
